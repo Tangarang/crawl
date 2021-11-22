cb8ee0927d69bc616611867fb1c7b53944f068ba
Patrick Lawson <pblawson00@gmail.com>
2021-11-21 19:16:53 -0500

    exit program

--- crawl-ref/source/options_menu.cc
+++ crawl-ref/source/options_menu.cc
@@ -1,8 +1,14 @@
 #include "options_menu.h"
+#include <string>
+#include "branch-type.h"
+#include <map>
+#include "skill-type.h"
+#include "externs.h"
+#include <memory>
+#include "end.h"
 #include <stdlib.h>
 // core program of options_menu
 
 void options_menu() {
-	// 
-	exit(0);
+    end(0);
 }
